3f2c27de01ff1c5f23130797c021920feab355ee
Dan Ambrisco <dambrisco@itriagehealth.com>
2015-05-25 18:29:39 -0600

    DRY up keybinds

--- keybindings/keybindings.go
+++ keybindings/keybindings.go
@@ -6,47 +6,29 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
+var gui *gocui.Gui
+
 func Set(g *gocui.Gui) {
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", gocui.KeyCtrlD, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'q', gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'h', gocui.ModNone, help); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'j', gocui.ModNone, cursorDown); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'k', gocui.ModNone, cursorUp); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'a', gocui.ModNone, upvote); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'z', gocui.ModNone, downvote); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", gocui.KeyEnter, gocui.ModNone, enter); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'l', gocui.ModNone, link); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'f', gocui.ModNone, front); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'c', gocui.ModNone, comments); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'r', gocui.ModNone, refresh); err != nil {
-		log.Panicln(err)
-	}
-	if err := g.SetKeybinding("", 'i', gocui.ModNone, info); err != nil {
+	gui = g
+	addBinding(gocui.KeyCtrlC, quit)
+	addBinding(gocui.KeyCtrlD, quit)
+	addBinding('q', quit)
+	addBinding('h', help)
+	addBinding('j', cursorDown)
+	addBinding('k', cursorUp)
+	addBinding('a', upvote)
+	addBinding('z', downvote)
+	addBinding(gocui.KeyEnter, enter)
+	addBinding('l', link)
+	addBinding('f', front)
+	addBinding('c', comments)
+	addBinding('r', refresh)
+	addBinding('i', info)
+	addBinding('y', info)
+}
+
+func addBinding(c interface{}, f gocui.KeybindingHandler) {
+	if err := gui.SetKeybinding("", c, gocui.ModNone, f); err != nil {
 		log.Panicln(err)
 	}
 }
